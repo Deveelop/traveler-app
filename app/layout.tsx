b122dcc7f854a1c4a1c756697be8097a3a7b9952
Victor Abuka <vicsonabuka@gmail.com>
2024-05-27 17:07:48 +0100

    first commit on Navbar component

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import React from "react";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
+
+export const metadata: Metadata = {
+  title: "Traveler Web App",
+  description: "Camping UI/UX App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className=" relative overflow-hidden">
+        {children}
+        </main>
+        <Footer/>
+        </body>
+    </html>
+  );
+}
