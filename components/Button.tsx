319133bd84de887ccc6310d5e25fb70e18e68c24
Victor Abuka <vicsonabuka@gmail.com>
2024-05-30 23:13:26 +0100

    Added new commits

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,17 +4,18 @@ type ButtonProbs = {
    type: 'button' | 'submit';
    title: string;
    icon?: string;
-   variant: string
-   onClick?: () => void
+   variant: string;
+   onClick?: () => void;
+   full?: boolean
 
    
 }
 
-const Button = ({type, title, icon, variant, onClick}: ButtonProbs) => {
+const Button = ({type, title, icon, variant, onClick, full}: ButtonProbs) => {
   return (
-    <button onClick={onClick} className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
+    <button onClick={onClick} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} type={type}>
       {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-     <label>{title}</label> 
+     <label className=" bold-16 whitespace-nowrap cursor-pointer">{title}</label> 
     </button>
   )
 }
